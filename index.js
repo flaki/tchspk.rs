e31a09b49aaad4cdecf36501bcd567795c0c7a66
Flaki <git@flaki.hu>
2017-01-03 10:03:47 +0100

    Automated daemon mode implemented

--- index.js
+++ index.js
@@ -11,223 +11,175 @@ const cfpTelegram = require('./lib/cfp-telegram.js');
 
 const CONFIG = require('./config.json');
 
-const DAYS_OF_WEEK = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+const STATE = {};
+
+try {
+  STATE.Q = require('./data/queue.json');
+} catch(e) { STATE.Q = {} };
+Object.defineProperty(STATE.Q, 'sync', { value: function() {
+  fs.writeFileSync('./data/queue.json', JSON.stringify(STATE.Q));
+}});
+
+try {
+  STATE.LD = require('./data/lastdate.json')
+} catch(e) { STATE.LD = { value: dates.getUTCDate('yesterday') } };
+Object.defineProperty(STATE.LD, 'next', { value: function() {
+  STATE.LD.value = dates.getUTCDate('tomorrow');
+  fs.writeFileSync('./data/lastdate.json', JSON.stringify(STATE.LD));
+}});
+
+// Mainloop loops every 60 seconds
+setInterval(mainLoop, 60000);
+
+mainLoop();
 
 
-//calendar.updateCfpData().then(cfp => {
-Promise.resolve(calendar.getCfpDataSync()).then(cfp => {
-  let dayOfWeek = dates.getDayOfWeek();
 
-  // Optionally modify the NOW date
-  let shiftDate = 0;
-  if (shiftDate) {
-    let shiftedDate = new Date().getTime()+24*60*60*1000*shiftDate;
+function mainLoop() {
+  // Current date
+  let d = dates.getUTCDate();
+  let ts = new Date().getTime();
 
-    console.log('Using this date:', new Date(shiftedDate).toUTCString());
+  // Datechange event
+  if (d > STATE.LD.value) {
+    loadQueue();
 
-    cfp = calendar.getCfpDataSync(shiftedDate);
-    dayOfWeek = dates.getDayOfWeek(shiftedDate);
+    // Increment state day reference
+    STATE.LD.next();
   }
 
-  // Weekly feed (every Sunday/Monday)
-  let isWeeklyFeed = (dayOfWeek === 1);
+  // Send
+  if (STATE.Q[d]) STATE.Q[d].filter(e => !e.sent && e.ts<ts).forEach(e => {
+    switch (e.t) {
+      case 'twitter':
+        sendTweet(e.msg);
+        break;
+      case 'telegram':
+        sendTelegram(e.msg);
+        break;
+    }
 
-  console.log('Day of week: ', dayOfWeek, '/', DAYS_OF_WEEK[dayOfWeek-1]);
-
-  console.log('Upcoming CFP dates:');
-
-  cfp = cfp
-    .filter(e => e.tsdiff>-1000*24*60*60)
-    .map(e => Object.assign(e, { parsed: parseEvent(e) }))
-  cfp.sort( (a,b) => a.ts-b.ts );
-
-  cfp.forEach(e => {
-    console.log(`[${e.daysToGoStr}]  ${e.summary}`);
-  });
-
-  let today = cfp.filter(e => e.daysToGo === 0);
-  let tomorrow = cfp.filter(e => e.daysToGo === 1);
-  let thisweek = cfp.filter(e => e.daysToGo <= (7-dayOfWeek));
+    e.sent = true;
+    STATE.Q.sync();
+  })
+}
 
 
-  // Find a highlighted event
-  let highlights;
+function loadQueue(shiftDate) {
+  calendar.updateCfpData().then(_ => {
+  //Promise.resolve().then(_ => {
 
-  // Has parsed location and a URL and @twitter
-  highlights = today.filter(e => e.parsed.loc && e.parsed.url && e.parsed.twitter);
-  // Has parsed location and a URL
-  highlights = today.filter(e => e.parsed.loc && e.parsed.url);
-  // Has at least a parsed URL
-  if (!highlights.length) highlights = today.filter(e => e.parsed.loc && e.parsed.url);
-  // Just show the first event from today
-  if (!highlights.length && today.length) highlights = [ today[0] ];
-  // Just show the first event from tomorrow
-  if (!highlights.length && tomorrow.length) highlights = [ tomorrow[0] ];
+    // Optionally modify the NOW date
+    let shiftedDate;
+    if (shiftDate) {
+      shiftedDate = new Date().getTime()+24*60*60*1000*shiftDate;
 
-  // No CFP due dates upcoming
-  if (!today.length && !tomorrow.length) {
-    if (isWeeklyFeed) {
-      // no weekly events either
-      if (!thisweek.length) {
-        console.log('No upcoming CFPs this week.');
+      console.log('Using this date:', new Date(shiftedDate).toUTCString());
+    }
+
+    // List upcoming CFPs
+    let events = calendar.listUpcomingCfps(shiftedDate);
+    let {
+      upcoming,
+      today, tomorrow, thisweek, highlights,
+      feed
+    } = events;
+
+
+    // Weekly feed (every Monday morning)
+    let isWeeklyFeed = (events.dayOfWeek === 1);
+
+    console.log('Day of week: ', events.dayOfWeek, '/', calendar.weekday(events.dayOfWeek-1));
+
+    // No CFP due dates upcoming
+    if (!today.length && !tomorrow.length) {
+      if (isWeeklyFeed) {
+        // no weekly events either
+        if (!thisweek.length) {
+          console.log('No upcoming CFPs this week.');
+          return;
+        }
+      } else {
+        console.log('No upcoming CFPs.');
         return;
       }
+    }
+
+    console.log('Upcoming CFP dates:');
+    console.log('Today:', today.length, 'Tomorrow:',tomorrow.length,'Highlights:', highlights.length);
+
+
+    // Empty feed
+    if (!feed.length) {
+      console.log('No events.');
+
     } else {
-      console.log('No upcoming CFPs.');
-      return;
-    }
-  }
+      // Twitter
+      if (CONFIG.TWITTER) {
+        let msgs = [];
 
-  console.log("Today:", today.length, "Tomorrow:",tomorrow.length,"Highlights:", highlights.length);
-
-
-  // Compile a feed that will be used to broadcast a reminder
-  let feed = [];
-  if (highlights.length) {
-    highlights[0].listedIn = 'highlights';
-    highlights[0].closesOn = highlights[0].daysToGo ? 'tomorrow' : 'today';
-    feed.push(highlights[0]);
-  }
-
-  today.forEach(e => {
-    if (!~feed.indexOf(e)) {
-      e.listedIn = 'today';
-      feed.push(e);
-    }
-  });
-
-  tomorrow.forEach(e => {
-    if (!~feed.indexOf(e)) {
-      e.listedIn = 'tomorrow';
-      feed.push(e);
-    }
-  });
-
-  if (isWeeklyFeed) {
-    thisweek.forEach(e => {
-      if (!~feed.indexOf(e)) {
-        e.listedIn = 'thisweek';
-        e.closesOn = `on ${DAYS_OF_WEEK[dates.getDayOfWeek(e.ts)-1]} (${e.daysToGoStr})`;
-        feed.push(e);
-      }
-    });
-  }
-
-  // Empty feed
-  if (!feed.length) {
-
-  } else {
-    let msg;
-    let fullFeed = feed;
-
-
-    // Twitter
-
-    // Weekly field (alt: close|end|due)
-    if (isWeeklyFeed) {
-      msg = '📢 '+ feed.length+' '+(feed.length>1 ?'CFPs close' :'CFP closes')+' this week:\n';
-
-      // Add highlights
-      let firstItem = true;
-      let mainFeed = feed.length <= 2 ? feed : feed.filter(e => e.listedIn === 'highlights');
-      mainFeed.forEach(e => {
-        // Snip this event from the feed
-        feed = feed.filter(snip => e !== snip);
-
-        let eName, eExtra;
-        if (firstItem) {
-          firstItem = false;
-
-          eName = e.parsed.twitter ? e.parsed.twitter[1] : e.parsed.conf;
-        } else {
-          eName = ', ' + (e.parsed.twitter ? e.parsed.twitter[1] : e.parsed.conf);
-          if ((msg+eName).length > 132) return;
+        // Weekly tweet
+        if (isWeeklyFeed) {
+          msgs.push( cfpTwitter.makeWeeklyTweet(feed) );
         }
 
-        msg += eName;
+        // Tweet daily for today's cfp deadlines
+        msgs = msgs.concat( cfpTwitter.makeTodaysTweets(events) );
 
-        // Extra info
-        eExtra = ` (due ${e.daysToGoStr})`;
-        if ( (msg+eExtra).length > 132 ) return;
+        msgs.forEach(msg => console.log(msg, [msg.length]));
+        //sendTweet(msg,true);
 
-        msg += eExtra;
-      });
-
-      // Prefer events with twitter tags
-      //feed.sort( (a,b) => { console.log(a.parsed.twitter?1:0,b.parsed.twitter?1:0); return (a.parsed.twitter?1:0)-(b.parsed.twitter?1:0) });
-
-      // Fill up with the rest of the feed items
-      feed.forEach(e => {
-        // Snip this event from the feed
-        feed = feed.filter(snip => e !== snip);
-
-        let eName;
-        if (firstItem) {
-          firstItem = false;
-
-          eName = e.parsed.twitter ? e.parsed.twitter[1] : e.parsed.conf;
-        } else {
-          eName = ', ' + (e.parsed.twitter ? e.parsed.twitter[1] : e.parsed.conf);
-          let ml = (msg+eName).length;
-
-          if (ml > 140) return
-
-          // Last item, if it fits 140 chars leave it
-          if (!feed.length === 0 || ml > 140) return;
-
-          // Leave 8 chars for "& more..."
-          if (ml > 132) return;
-        }
-
-        msg += eName;
-      });
-
-      // If there are more still
-      if (feed.length) {
-        msg += ' & more…';
-      } else {
-        if (msg.length<140) msg+='.';
+        enqueue('twitter', msgs);
       }
 
-      // TODO: attach picture "calendar rendering" for the week
+      // Telegram
+      if (CONFIG.TELEGRAM) {
+        let msg = cfpTelegram.makeMessage(events);
 
-      console.log(msg, [msg.length]);
-      //sendTweet(msg,true);
-    }
+        console.log(msg);
 
-
-    // Telegram
-    msg = '';
-    feed = fullFeed;
-    if (feed[0].listedIn === 'highlights') {
-      msg += formatHighlight(feed[0]);
-      feed = feed.slice(1);
-
-      if (feed.length>0) msg += '\n\nFurther call for proposals';
-    } else {
-      msg += '📢 Call for proposals';
-    }
-
-    if (feed.length > 0) {
-      if (isWeeklyFeed) {
-        msg += ' due this week:';
-      } else {
-        msg += ' ending soon:';
+        enqueue('telegram', msg);
+        //sendTelegram(msg,true);
       }
-
-      feed.forEach(e => {
-        msg += `\n🗣 ${e.parsed.title} ${e.listedIn==='today'?'also closes':'closes'} ${e.closesOn||e.listedIn}`;
-      });
-
-      //msg += '\n\n'+feed[0].parsed.url;
     }
 
-    console.log(msg);
-    //sendTelegram(msg,true);
+  }).catch(e => console.log(e.stack||e));
+}
+
+
+function enqueue(type, msgs) {
+  let key = dates.getUTCDate();
+
+  let q = STATE.Q[key];
+  if (!q) {
+    STATE.Q[key] = q = [];
   }
 
-}).catch(e => console.log(e.stack||e));
+  let ts;
+  switch (type) {
+    case 'twitter':
+      ts = dates.dateTimeUTC(undefined, undefined, undefined, undefined, CONFIG.TWITTER.SEND_HOUR, CONFIG.TWITTER.SEND_MINS);
 
+      msgs.forEach(msg => {
+        q.push({ t: type, msg: msg, ts: ts });
+        ts += CONFIG.TWITTER.SEND_INTERVAL;
+      });
+      break;
+
+    case 'telegram':
+      ts = dates.dateTimeUTC(undefined, undefined, undefined, undefined, CONFIG.TELEGRAM.SEND_HOUR, CONFIG.TELEGRAM.SEND_MINS);
+
+      q.push({
+        t: type,
+        msg: msgs,
+        ts: ts
+      });
+      break;
+  }
+
+  console.log('Queue updated..', STATE.Q);
+  STATE.Q.sync();
+}
 
 function sendTweet(msg, test) {
   if (test) {
@@ -245,46 +197,4 @@ function sendTelegram(msg, test) {
 
   console.log('TELEGRAM'+(test?' TEST':'')+':', req);
   return fetch(req).catch(e => console.log(e.stack||e));
-}
-
-function parseEvent(e) {
-  let title = e.summary.replace(/\([^\)]*\)/g,'').trim();
-  // Clean extra info from title
-
-  // If "CFP" is not specified in the title itself, add it
-  if (!~title.toUpperCase().indexOf('CFP')) title+=' CFP';
-
-  // Conf name without the "CFP" part
-  let conf = title.replace(/\s+CFP/,'').trim();
-
-  // Is there an URL in the description?
-  let url = e.description.match(/http[s]?\:\/\/\S+/);
-
-  // Is there an URL in the description?
-  let twitter = e.description.match(/(?:^|\s)(\@\w+)/);
-
-  // Match place and date
-  let loc = e.summary.match(/\(([^\)]+)\)/);
-
-  return ({
-    title, conf, url, loc, twitter
-  });
-}
-
-function formatHighlight(e) {
-  let { title, url, loc, twitter } = e.parsed;
-  let when = e.listedIn === 'highlights' ? (e.closesOn||'today') : (e.closesOn||e.listedIn);
-
-  let ret = `📢 The ${title} closes ${when}!`;
-
-  if (loc) {
-    ret += `\n📆 ${loc[1]}`;
-  }
-  if (twitter && twitter[1]) {
-    ret += `\n🐦 ${twitter[1]}`;
-  }
-  if (url) {
-    ret += `\n${url[0]}`;
-  }
-  return ret;
 }
